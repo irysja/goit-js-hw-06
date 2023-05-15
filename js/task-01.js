a3203b8efd5b7b172f4dd20fadb303505bb6b9fb
irysja <irysja.gyikova@gmail.com>
2023-05-15 12:40:44 +0300

    error correction

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,5 @@
-const categories = document.querySelector('#categories');
-const items = categories.querySelectorAll('.item');
+
+const items = document.querySelectorAll('.item');
 console.log(`Number of categories: ${items.length}`);
 items.forEach(item => {
     const title = item.querySelector('h2').textContent;
--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,7 @@
+
 const fontSizeControl = document.querySelector('#font-size-control');
 const text = document.querySelector('#text');
+text.style.fontSize = `${fontSizeControl.value}px`;
 fontSizeControl.addEventListener('input', () => {
-    text.style.fontSize =` ${fontSizeControl.value}px`;
-});
+    text.style.fontSize = `${fontSizeControl.value}px`;
+});1233123
